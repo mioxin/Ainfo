6058f68d37ebf3d699b5392e8a56b00e6c223caf
MrMioxin <Mioxin@ya.ru>
2015-04-29 18:06:09 +0600

    get stackoverflow info

---
+++ app/src/main/java/com/gmail/mrmioxin/ainfo/HtmlHelper.java
@@ -0,0 +1,45 @@
+package com.gmail.mrmioxin.ainfo;
+
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by palchuk on 29.04.2015.
+ */
+public class HtmlHelper {
+    TagNode rootNode;
+
+    //Конструктор
+    public HtmlHelper(URL htmlPage) throws IOException
+    {
+        //Создаём объект HtmlCleaner
+        HtmlCleaner cleaner = new HtmlCleaner();
+        //Загружаем html код сайта
+        rootNode = cleaner.clean(htmlPage);
+    }
+
+    List<TagNode> getLinksByClass(String CSSClassname)
+    {
+        List<TagNode> linkList = new ArrayList<TagNode>();
+
+        //Выбираем все ссылки
+        TagNode linkElements[] = rootNode.getElementsByName("a", true);
+        for (int i = 0; linkElements != null && i < linkElements.length; i++)
+        {
+            //получаем атрибут по имени
+            String classType = linkElements[i].getAttributeByName("class");
+            //если атрибут есть и он эквивалентен искомому, то добавляем в список
+            if (classType != null && classType.equals(CSSClassname))
+            {
+                linkList.add(linkElements[i]);
+            }
+        }
+
+        return linkList;
+    }
+}
---
+++ app/src/main/java/com/gmail/mrmioxin/ainfo/MainActivity.java
@@ -0,0 +1,106 @@
+package com.gmail.mrmioxin.ainfo;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.htmlcleaner.TagNode;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String MY_LOG = "My_log";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Находим кнопку
+        Button button = (Button)findViewById(R.id.parse);
+        //Регистрируем onClick слушателя
+        button.setOnClickListener(myListener);
+    }
+
+    //Диалог ожидания
+    private ProgressDialog pd;
+    //Слушатель OnClickListener для нашей кнопки
+    private View.OnClickListener myListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            //Показываем диалог ожидания
+            pd = ProgressDialog.show(MainActivity.this, "Working...", "request to server", true, false);
+            //Запускаем парсинг
+            new ParseSite().execute("http://www.stackoverflow.com");
+        }
+    };
+
+    private class ParseSite extends AsyncTask<String, Void, List<String>> {
+        //Фоновая операция
+        protected List<String> doInBackground(String... arg) {
+            List<String> output = new ArrayList<String>();
+            try
+            {
+                HtmlHelper hh = new HtmlHelper(new URL(arg[0]));
+                List<TagNode> links = hh.getLinksByClass("question-hyperlink");
+                Log.d(MY_LOG, links.toString());
+
+                for (Iterator<TagNode> iterator = links.iterator(); iterator.hasNext();)
+                {
+                    TagNode divElement = (TagNode) iterator.next();
+                    output.add(divElement.getText().toString());
+                    //Log.d(MY_LOG, divElement.getText().toString());
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return output;
+        }
+
+        //Событие по окончанию парсинга
+        protected void onPostExecute(List<String> output) {
+            //Убираем диалог загрузки
+            pd.dismiss();
+            //Находим ListView
+            ListView listview = (ListView) findViewById(R.id.listViewData);
+            //Загружаем в него результат работы doInBackground
+            listview.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1 , output));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
